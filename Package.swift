f26f38b1631d007bd7f8a0951db8feac6595cfe4
Alex Wishart <awishart@uk.ibm.com>
2018-06-07 13:45:51 +0100

    Update versions

--- Package.swift
+++ Package.swift
@@ -21,11 +21,11 @@ import PackageDescription
 let package = Package(
     name: "Kitura-Server",
     dependencies: [
-      .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.0.0")),
-      .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.1")),
+      .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.0.0"),
+      .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.7.1"),
       .package(url: "https://github.com/IBM-Swift/Configuration.git", from: "3.0.0"),
       .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
-      .package(url: "https://github.com/IBM-Swift/Health.git", from: "0.0.0"),
+      .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.0"),
     ],
     targets: [
       .target(name: "Kitura-Server", dependencies: [ .target(name: "Application"), "Kitura" , "HeliumLogger"]),
