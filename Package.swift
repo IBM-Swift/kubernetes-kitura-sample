bf44af7ca6fba71713b718256c634816996fc4dd
Alex Wishart <awishart@uk.ibm.com>
2018-06-07 16:23:38 +0100

    Kitura version >= 2.4.0

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-Server",
     dependencies: [
-      .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.0.0"),
+      .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.4.0"),
       .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.7.1"),
       .package(url: "https://github.com/IBM-Swift/Configuration.git", from: "3.0.0"),
       .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
